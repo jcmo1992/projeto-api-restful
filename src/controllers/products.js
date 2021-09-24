fe54162c1cb4dcf58e483c2b2ef86a4ed89c69a2
Julio <julio.oliveira@acpgroup.com.br>
2021-09-24 14:11:51 -0300

    Adicionado metodo delete

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -51,9 +51,22 @@ async function put(req, res) {
     // });
 }
 
+async function remove(req, res) {
+    const { id } = req.params;
+
+    const remove = await ProductsModel.deleteOne({ _id: id});
+
+    const message = remove.ok ? 'Sucesso' : 'Error';
+
+    res.send({
+        message
+    })
+}
+
 
 module.exports = {
     get,
     post, 
-    put
+    put,
+    remove
 }
